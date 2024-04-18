ad026080f5e521aee9a1cf1f9ac6895ef483b2e7
Mogy <norou6@gmail.com>
2024-04-18 09:55:26 +0100

    home page

--- Client-Side/src/component/footer/Footer.jsx
+++ Client-Side/src/component/footer/Footer.jsx
@@ -3,7 +3,105 @@ import "./Footer.scss"
 const Footer  = () => {
   return (
       <footer className="footer">
-          <div className="container">Footer</div>
+          <div className="container">
+              <div className="top">
+                  <div className="item">
+                      <h2>Categories</h2>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                  </div>
+                  <div className="item">
+                      <h2>Categories</h2>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                  </div>
+                  <div className="item">
+                      <h2>Categories</h2>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                  </div>
+                  <div className="item">
+                      <h2>Categories</h2>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                  </div>
+                  <div className="item">
+                      <h2>Categories</h2>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                      <span>Drilling Category</span>
+                  </div>
+              </div>
+              <hr/>
+              <div className="bottom">
+                  <div className="left">
+                      <h2>Drill</h2>
+                      <span>&#9400; Drill National Ltd, 2024</span>
+                  </div>
+                  <div className="right">
+                      <div className="social">
+                          <img src="./images/icons8-linkedin-30.png" alt=""/>
+                          <img src="./images/icons8-youtube-24.png" alt=""/>
+                          <img src="./images/icons8-twitterx-50.png" alt=""/>
+                          <img src="./images/icons8-instagram-50.png" alt=""/>
+                          <img src="./images/icons8-pinterest-50.png" alt=""/>
+                      </div>
+                      <div className="link">
+                          <img src="./images/icons8-language-50.png" alt=""/>
+                          <span>English</span>
+                      </div>
+                      <div className="link">
+                          <img src="./images/icons8-coin-50.png" alt=""/>
+                          <span>DH</span>
+                      </div>
+                      <img src="./images/icons8-accesibility-48.png" alt=""/>
+                  </div>
+              </div>
+          </div>
       </footer>
   )
 }
