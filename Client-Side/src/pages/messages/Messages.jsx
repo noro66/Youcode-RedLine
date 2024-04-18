820315953d03fd03618e8573ddc05a9a129cc07c
Mogy <norou6@gmail.com>
2024-04-18 20:24:36 +0100

    messages

--- Client-Side/src/pages/messages/Messages.jsx
+++ Client-Side/src/pages/messages/Messages.jsx
@@ -1,5 +1,6 @@
 
 import './Messages.scss'
+import {Link} from "react-router-dom";
 const Messages = () => {
 
     const currentUser = {
@@ -7,10 +8,12 @@ const Messages = () => {
         username: "john doe",
         isSeller: true
     }
+    const message = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. A accusamus alias amet assumenda doloremque error impedit magnam nam nemo quae recusandae reprehenderit, rerum tenetur vel voluptatem! Atque beatae explicabo optio. ';
 return (
     <div className={'messages'}>
         <div className="container">
             <div className="title">
+
                 <h1>Messages</h1>
             </div>
             <table>
@@ -19,17 +22,21 @@ return (
                     <th>Client</th>
                     <th>Last Message</th>
                     <th>Date</th>
-                    <th>{currentUser?.isSeller ? 'Client' : 'Service Provider'}</th>
                     <th>Action</th>
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
+                <tr className={'active'}>
                     <td>John Doe</td>
-                    <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, dicta doloremque perspiciatis
-                        praesentium quo voluptatem.
-                    </td>
+                    <td><Link className={'link'} to={'/message/12'}> {message.substring(0, 100)}..</Link> </td>
                     <td>1 day ago</td>
+                    <td>
+                        <button>Mark As Read</button>
+                    </td>
+                </tr>
+                <tr className={'active'}>
+                    <td>John Doe</td>
+                    <td><Link className={'link'} to={'/message/12'}> {message.substring(0, 100)}..</Link> </td>
                     <td>1 day ago</td>
                     <td>
                         <button>Mark As Read</button>
@@ -37,47 +44,18 @@ return (
                 </tr>
                 <tr>
                     <td>John Doe</td>
-                    <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, dicta doloremque perspiciatis
-                        praesentium quo voluptatem.
-                    </td>
+                    <td><Link className={'link'} to={'/message/12'}> {message.substring(0, 100)}..</Link> </td>
                     <td>1 day ago</td>
-                    <td>1 day ago</td>
-                    <td>
-                        <button>Mark As Read</button>
-                    </td>
                 </tr>
                 <tr>
                     <td>John Doe</td>
-                    <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, dicta doloremque perspiciatis
-                        praesentium quo voluptatem.
-                    </td>
+                    <td><Link className={'link'} to={'/message/12'}> {message.substring(0, 100)}..</Link> </td>
                     <td>1 day ago</td>
-                    <td>1 day ago</td>
-                    <td>
-                        <button>Mark As Read</button>
-                    </td>
                 </tr>
                 <tr>
                     <td>John Doe</td>
-                    <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, dicta doloremque perspiciatis
-                        praesentium quo voluptatem.
-                    </td>
+                    <td><Link className={'link'} to={'/message/12'} > {message.substring(0, 100)}..</Link> </td>
                     <td>1 day ago</td>
-                    <td>1 day ago</td>
-                    <td>
-                        <button>Mark As Read</button>
-                    </td>
-                </tr>
-                <tr>
-                    <td>John Doe</td>
-                    <td>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, dicta doloremque perspiciatis
-                        praesentium quo voluptatem.
-                    </td>
-                    <td>1 day ago</td>
-                    <td>1 day ago</td>
-                    <td>
-                        <button>Mark As Read</button>
-                    </td>
                 </tr>
                 </tbody>
             </table>
