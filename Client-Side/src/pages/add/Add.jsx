afe1ee4b075a49979b112cba35058a3b389ff7d1
Mogy <norou6@gmail.com>
2024-04-27 01:22:10 +0100

    service creation validation

--- Client-Side/src/pages/add/Add.jsx
+++ Client-Side/src/pages/add/Add.jsx
@@ -1,65 +1,78 @@
-import './Add.scss'
-import {useReducer, useState} from "react";
-import {INITIAL_STATE, serviceReducer} from "../../reducers/serviceReducer.js";
-import {useQuery} from "@tanstack/react-query";
+import './Add.scss';
+import { useForm } from 'react-hook-form';
+import { useQuery } from "@tanstack/react-query";
 import customAxios from "../../../CustomAxios.js";
 
 const Add = () => {
-    const { isPending, isLoading, error, data: categories, refetch} = useQuery({ queryKey: ['categories'],
-        queryFn: ()=> customAxios.get('home').then(res => res.data.categories) });
+    const { isPending, isLoading, error, data: categories, refetch } = useQuery({ queryKey: ['categories'],
+        queryFn: () => customAxios.get('home').then(res => res.data.categories) });
 
-    const  [singleFile, setSingleFile] = useState(undefined);
-    const [files, setFiles] = useState([]);
-    const [uploading, setUploading] = useState(false);
+    const { register, handleSubmit, formState: { errors } } = useForm();
 
-    const  [state, dispatch] = useReducer(serviceReducer ,INITIAL_STATE)
-return (
-    <div className={'add'}>
-        <div className="container">
-            <h1>Add New Service</h1>
-            <div className="sections">
-                <div className="left">
-                    <label htmlFor="">Title</label>
-                    <input type="text" placeholder={'e.g I will provide a good drilling service'}/>
-                    <label htmlFor="">Category</label>
-                    <select name="cats" id="cats">
-                        <option selected={true} value="">Select Category </option>
-                        {
-                            isPending ? "loading..." : error ? "error" :
-                                categories?.map((cat) => <option value={cat?.id}>{cat?.title}</option>
-                                )
-                        }
-                    </select>
-                    <label htmlFor="">Cover Image</label>
-                    <input type="file" id="coverImage" name="coverImage"/>
-                    <label htmlFor="">Upload Images</label>
-                    <input type="file" id="serviceImages" multiple name="serviceImages"/>
-                    <label htmlFor="">Description</label>
-                    <textarea name="desc" id="" placeholder='Brief description to introduce  your service  to Clients' cols="30" rows="16"></textarea>
-                    <button>Create</button>
-                </div>
-                <div className="right">
-                    <label htmlFor="">Service Title</label>
-                    <input type="text" id="serviceTitle" name="serviceTitle" placeholder="Service Title e.g 250 metter drilling"/>
-                    <label htmlFor="">Short Description</label>
-                    <textarea name="" id="" placeholder='Short description of your service' cols="30" rows="10"></textarea>
-                    <label htmlFor="">Delivery Time (e.g 3 days)</label>
-                    <input type="number"  min={1}/>
-                    <label htmlFor="">Revision Number</label>
-                    <input type="number"  min={1}/>
-                    <label htmlFor="">Add Features</label>
-                    <input type="text" placeholder='features ...'/>
-                    <input type="text" placeholder='features ...' />
-                    <input type="text" placeholder='features ...' />
-                    <input type="text" placeholder='features ...' />
-                    <input type="text" placeholder='features ...' />
-                    <label htmlFor="">Price</label>
-                    <input type="number" min={1}/>
-                </div>
+    const onSubmit = data => {
+        console.log(data);
+    };
+
+    return (
+        <div className={'add'}>
+            <div className="container">
+                <h1>Add New Service</h1>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="sections">
+                        <div className="left">
+                            <label htmlFor="title">Title</label>
+                            <input type="text" {...register("title", { required: 'Title is required' })} placeholder={'e.g., I will provide a good drilling service'} />
+                            {errors.title && <p>{errors.title.message}</p>}
+
+                            <label htmlFor="category">Category</label>
+                            <select name="cats" id="cats" {...register("category", { required: 'Category is required' })}>
+                                <option value="">Select Category</option>
+                                {isPending ? "Loading..." : error ? "Error" :
+                                    categories?.map((cat) => <option key={cat?.id} value={cat?.id}>{cat?.title}</option>)
+                                }
+                            </select>
+                            {errors.category && <p>{errors.category.message}</p>}
+
+                            <label htmlFor="coverImage">Cover Image</label>
+                            <input type="file" id="coverImage" name="coverImage" {...register("coverImage", { required: 'cover Image is required' })} />
+
+                            <label htmlFor="serviceImages">Upload Images</label>
+                            <input type="file" id="serviceImages" multiple name="serviceImages" {...register("serviceImages",  { required: 'at least one image is required' })} />
+
+                            <label htmlFor="desc">Description</label>
+                            <textarea name="desc" id="desc" {...register("desc", { required: 'Description is required' })} placeholder='Brief description to introduce your service to Clients' cols="30" rows="16"></textarea>
+                            {errors.desc && <p>{errors.desc.message}</p>}
+
+                            <button type="submit">Create</button>
+                        </div>
+                        <div className="right">
+                            <label htmlFor="serviceTitle">Service Title</label>
+                            <input type="text" id="serviceTitle" name="serviceTitle" {...register("serviceTitle", { required: 'Service title is required' })} placeholder="Service Title e.g., 250 meter drilling" />
+                            {errors.serviceTitle && <p>{errors.serviceTitle.message}</p>}
+
+                            <label htmlFor="shortDescription">Short Description</label>
+                            <textarea name="shortDescription" id="shortDescription" {...register("shortDescription")} placeholder='Short description of your service' cols="30" rows="10"></textarea>
+
+                            <label htmlFor="deliveryTime">Delivery Time (e.g., 3 days)</label>
+                            <input type="number" min={1} {...register("deliveryTime")} />
+
+                            <label htmlFor="revisionNumber">Revision Number</label>
+                            <input type="number" min={1} {...register("revisionNumber")} />
+
+                            <label htmlFor="features">Add Features</label>
+                            {Array.from({ length: 5 }, (_, index) => (
+                                <input key={index} {...register(`features[${index}]`)} type="text" placeholder="Feature" />
+                            ))}
+
+                            <label htmlFor="price">Price</label>
+                            <input type="number" min={1} {...register("price", { required: 'Price is required and must be greater than 0' })} />
+                            {errors.price && <p>{errors.price.message}</p>}
+                        </div>
+                    </div>
+                </form>
             </div>
         </div>
-    </div>
-)
+    )
 };
 
 export default Add;
