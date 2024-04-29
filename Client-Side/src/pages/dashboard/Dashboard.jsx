7fbb698da9e346c144e8c3a8dac79d34fd4d601d
Mogy <norou6@gmail.com>
2024-04-29 15:01:19 +0100

    the admin avatar with logout

--- Client-Side/src/pages/dashboard/Dashboard.jsx
+++ Client-Side/src/pages/dashboard/Dashboard.jsx
@@ -1,22 +1,28 @@
-
+import React from 'react';
 import './Dashboard.scss';
-import {Link, Outlet} from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
     return (
         <div className="dashboard">
-            <header className="header"></header>
+            <header className="header">
+                <div className="profile">
+                    <img src="https://via.placeholder.com/50" alt="Profile" />
+                    <span>Username</span>
+                </div>
+            </header>
             <section className="sidebar">
                 <nav>
                     <ul>
-                        <li><Link to="/dashboard/statistics">statistics</Link></li>
+                        <li><Link to="/dashboard/statistics">Statistics</Link></li>
                         <li><Link to="/dashboard/users">Users</Link></li>
                         <li><Link to="/dashboard/approve">Approve Services</Link></li>
+                        <button className="logout">Logout</button>
                     </ul>
                 </nav>
             </section>
             <main className="main">
-                <Outlet/>
+                <Outlet />
             </main>
         </div>
     );
