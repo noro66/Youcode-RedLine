db5d761029667b5d1e5cfffe3c0202835ebe63aa
Mogy <norou6@gmail.com>
2024-04-29 15:20:00 +0100

    logout from the dashboard

--- Client-Side/src/pages/dashboard/Dashboard.jsx
+++ Client-Side/src/pages/dashboard/Dashboard.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
 import './Dashboard.scss';
-import { Link, Outlet } from "react-router-dom";
+import {Link, Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../../context/ContextProvider.jsx";
+import customAxios from "../../../CustomAxios.js";
+
 
 const Dashboard = () => {
+    const  {user, token, setUser, setToken} = useStateContext();
+
+    function handelLogout() {
+        if (confirm("Are you sure?")) {
+            customAxios.post("auth/logout", {token: token}).then(() =>  {
+                setToken(null);
+                setUser(null);
+                sessionStorage.removeItem("token");
+                sessionStorage.removeItem("user");
+                navigate('/');
+            }).catch(r => console.log(r));
+        }
+    }
+
+   if (!token){
+       return  <Navigate to="/login"/>;
+   }
     return (
         <div className="dashboard">
             <header className="header">
                 <div className="profile">
-                    <img src="https://via.placeholder.com/50" alt="Profile" />
-                    <span>Username</span>
+                    <img src={user?.img} alt="Profile" />
+                    <span>{user?.username}</span>
                 </div>
             </header>
             <section className="sidebar">
@@ -17,7 +37,7 @@ const Dashboard = () => {
                         <li><Link to="/dashboard/statistics">Statistics</Link></li>
                         <li><Link to="/dashboard/users">Users</Link></li>
                         <li><Link to="/dashboard/approve">Approve Services</Link></li>
-                        <button className="logout">Logout</button>
+                        <button onClick={handelLogout} className="logout">Logout</button>
                     </ul>
                 </nav>
             </section>
