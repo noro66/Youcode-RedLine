4b50070567a50c27a85d4b169550a61a7272634e
Mogy <norou6@gmail.com>
2024-04-29 14:49:28 +0100

    Admin Dashboard

--- Client-Side/src/pages/dashboard/Dashboard.jsx
+++ Client-Side/src/pages/dashboard/Dashboard.jsx
@@ -1,12 +1,20 @@
 
 import './Dashboard.scss';
-import {Outlet} from "react-router-dom";
+import {Link, Outlet} from "react-router-dom";
 
 const Dashboard = () => {
     return (
         <div className="dashboard">
             <header className="header"></header>
-            <section className="sidebar"></section>
+            <section className="sidebar">
+                <nav>
+                    <ul>
+                        <li><Link to="/dashboard/statistics">statistics</Link></li>
+                        <li><Link to="/dashboard/users">Users</Link></li>
+                        <li><Link to="/dashboard/approve">Approve Services</Link></li>
+                    </ul>
+                </nav>
+            </section>
             <main className="main">
                 <Outlet/>
             </main>
